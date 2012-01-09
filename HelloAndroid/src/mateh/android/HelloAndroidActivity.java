c2cbbc845d3fa0ab0bdb3ae0541c4cac80411b2a
Matthew Chou <mateh.cc@gmail.com>
2012-01-09 18:21:38 -0500

    update

--- HelloAndroid/src/mateh/android/HelloAndroidActivity.java
+++ HelloAndroid/src/mateh/android/HelloAndroidActivity.java
@@ -12,6 +12,6 @@ public class HelloAndroidActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main);//
     }
 }
